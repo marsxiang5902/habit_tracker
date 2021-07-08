5f0ea3d87ef438ade27d4f70b1c395b2c84c5c11
Adam <adamomarali37@gmail.com>
2021-07-08 10:48:14 -0400

    cleaned up pages, seperated components

--- client/src/pages/all.js
+++ client/src/pages/all.js
@@ -4,6 +4,7 @@ import { Form } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as Icons from "react-icons/fa";
 import MyHabitForm from '../components/form'
+import HabitList from '../components/habit-list'
 
 
 const pct = (items) => {
@@ -30,8 +31,6 @@ function All(props) {
     const [habitFormVisible, setHabitFormVisible] = useState(false)
     const [habitName, setHabitName] = useState("")
 
-    const temp = props.addHabit
-
     function handleSubmit(event){
         event.preventDefault();
         console.log(habitName)
@@ -42,54 +41,7 @@ function All(props) {
 
     return (
         <>
-            <div className="container">
-                <div className="subheader">
-                    <h2>Daily Habits</h2>
-                    {!habitFormVisible ? 
-                    <Icons.FaRegPlusSquare onClick={() => {setHabitFormVisible(true)}} className="hover"></Icons.FaRegPlusSquare> : 
-                    <Icons.FaRegWindowClose onClick={() => {setHabitFormVisible(false)}} className="hover"></Icons.FaRegWindowClose>}
-                </div>
-                {props.habits.map((item, index) => {
-                    return (
-                        <div className="card-2 border-2" key={index}>
-
-                            {/* <h4 className="habit">{pct(item.done)}%</h4> */}
-                            <div className="habit habit-2">
-                                <h5>{item.name}</h5>
-                                {/* <p>{item.description}</p> */}
-                            </div>
-                        </div>
-                    );
-                })}
-                {props.addedHabits.map((item, index) => {
-                    return (
-                        <div className="card-2 border-2" key={index}>
-
-                            {/* <h4 className="habit">{pct(item.done)}%</h4> */}
-                            <div className="habit habit-2">
-                                <h5>{item.name}</h5>
-                                {/* <p>{item.description}</p> */}
-                            </div>
-                        </div>
-                    );
-                })}
-                {habitFormVisible ? 
-                <div className="card-2 border-2">
-                    <div className="form-padding">
-                        {/* <MyHabitForm habitName={habitName} setName={setHabitName} visible={setHabitFormVisible} updateHabits={props.addHabit}/> */}
-                        <Form onSubmit={handleSubmit}>
-                            <Form.Group>
-                                <Form.Label>Add a New Habit</Form.Label>
-                                <Form.Control type="text" placeholder="Habit Name" value={habitName} onChange={(e) => setHabitName(e.target.value)}/>
-                                <Form.Text className="text-muted">
-                                Example - Meditate 15 minutes per day
-                                </Form.Text>
-                            </Form.Group>
-                        </Form>
-                    </div>
-                </div> : null}
-
-            </div>
+            <HabitList addedHabits={props.addedHabits} habits={props.habits} addHabit={props.addHabit}/>
         </>
     );
 }
