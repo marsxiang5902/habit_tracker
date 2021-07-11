ab0e504427f32842a729e2aa5b50e3cc6f9ea56a
Adam <adamomarali37@gmail.com>
2021-07-11 17:03:09 -0400

    Habit api setup

--- client/src/pages/habits.js
+++ client/src/pages/habits.js
@@ -6,17 +6,9 @@ function Habits(props){
     return(
         <>
             {props.habits.map((item, index) => {
-                let done = item.done[(item.done.length) - 1]
-                if (done == 0) {
-                    done = false
-                }
-                else {
-                    done = true
-                }
                         return (
                             <div className="habit-card">
                                 <Checkbox
-                                    checked={done}
                                     // onChange={e => setChecked(e.target.checked)}
                                     />
                                     <div className="habit" key={index}>
