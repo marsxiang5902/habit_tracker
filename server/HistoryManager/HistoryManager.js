171175f696a403cbf98a01176270779209077392
marsxiang5902 <marsxiang5902@gmail.com>
2021-07-11 18:10:14 -0400

    aligned historymanager date to local date

--- server/HistoryManager/HistoryManager.js
+++ server/HistoryManager/HistoryManager.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const assert = require('assert')
-const MILLS_IN_DAY = 1000 * 60 * 60 * 24
+const MILLS_IN_MIN = 1000 * 60, MILLS_IN_DAY = MILLS_IN_MIN * 60 * 24
 const getDay = function (date = new Date()) {
-    return Math.floor(date.getTime() / MILLS_IN_DAY)
+    return Math.floor((date.getTime() - date.getTimezoneOffset() * MILLS_IN_MIN) / MILLS_IN_DAY)
 }
 const checkData = function (data, minDaysBefore = 0, maxDaysBefore = 1000000) {
     // check if data is in the form of a object: {daysbefore: data}
@@ -30,5 +30,5 @@ module.exports = {
             return [];
         }
         static setHistory = function setHistory() { }
-    }, MILLS_IN_DAY, getDay, checkData
+    }, MILLS_IN_MIN, MILLS_IN_DAY, getDay, checkData
 }
