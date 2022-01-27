892d111d00f2bc77e0b98efec06eef9de5005eec
marsxiang5902 <marsxiang5902@gmail.com>
2022-01-26 22:40:15 -0500

    groups on frontend (not finished yet)

--- client/src/pages/Accountability.js
+++ client/src/pages/Accountability.js
@@ -1,42 +1,80 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { appContext } from "../context/appContext";
 import '../static/page.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/layout";
+import { Button, Form } from "react-bootstrap";
+import { addGroup } from "../services/groupServices";
+
+function GroupMember(props) {
+    return <h5>{props.name}</h5>
+}
+
+function GroupCardAddNew(props) {
+    let context = useContext(appContext)
+    let [adding, setAdding] = useState(false)
+    let [name, setName] = useState("")
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        props.setContext(await addGroup(context, name))
+        setAdding(false)
+        setName("")
+    }
+
+    return adding ? <div className="groups-item">
+        <div className="form-padding">
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>{"Add A New Group"}</Form.Label>
+                    <Form.Control type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                    <Button className="button" variant="success" type="submit" value='add'>Add</Button>
+                    <Button className="button" variant="danger" value='cancel' onClick={() => {
+                        setName(""); setAdding(false);
+                    }}>Cancel</Button>
+                </Form.Group>
+            </Form>
+        </div>
+    </div> : <div className="groups-item groups-add"><h2 onClick={() => setAdding(true)}>+ Add Group</h2></div>
+}
 
 function GroupCard(props) {
-    let groupRecord = props.groupRecord, groupEvents = props.groupEvents
-    return null
+    let groupRecord = props.groupRecord
+    return <div className="groups-item">
+        <h3>{groupRecord.name}</h3>
+        <p>{`${Object.keys(groupRecord.members).length} member(s)`}</p>
+        <div className="divider" />
+        {Object.keys(groupRecord.members).map((name, idx) => idx > 2 ? null : <GroupMember name={name} />)}
+    </div>
 }
 
 export default function Accountability(props) {
     let context = useContext(appContext)
-    // let partnerUndone = context.partnerUndone
-
-    // let allUndone = []
-    // for (let type in partnerUndone) {
-    //     for (let name of partnerUndone[type]) {
-    //         allUndone.push(name)
-    //     }
-    // }
-
     return (
         <div className="wrapper">
-            <Layout name="🗺 THE USER" handleLogout={props.handleLogout} menu={props.menu} showMenu={props.showMenu} />
-            <div className="dashboard">
-
-                <GroupCard />
-                {/* <div class="fullheader">
-                    <h2>{`${context.session.partner}'s Undone Events`}</h2>
-                </div> */}
-
-                {/* {allUndone.map((name, idx) => (
-                    <div className="card-2 border-2" key={idx}>
-                        <div className="habit habit-2 inline">
-                            <h5 className="habit no-padding-top">{name}</h5>
-                        </div>
-                    </div>))} */}
-
+            <Layout name="🗺 GROUPS" handleLogout={props.handleLogout} menu={props.menu} showMenu={props.showMenu} />
+            <div className={props.menu ? "main-content active" : "main-content"}>
+                <div className="dashboard">
+                    <div className="groups-grid-container">
+                        {Object.keys(context.session.groups).map(_id => <GroupCard groupRecord={context.session.groups[_id]} />)}
+                        {[...Array(0).keys()].map(v => <GroupCard groupRecord={{
+                            "_id": "61f01d8ab15f520016b77012",
+                            "user": "test2",
+                            "name": "Group 1",
+                            "members": {
+                                "test2": []
+                            },
+                            "roles": {
+                                "test2": [
+                                    "default",
+                                    "admin"
+                                ]
+                            },
+                            "invites": []
+                        }} />)}
+                        <GroupCardAddNew setContext={props.setContext} />
+                    </div>
+                </div>
             </div>
         </div>
     );
